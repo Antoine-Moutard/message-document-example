37ae5b2262533bbb9a4dd7c026370c09a7ddd971
Antoine-Moutard <amoutard368@gmail.com>
2024-11-03 14:46:08 +0100

    fix

--- src/main/java/fr/miage/CamelRoute.java
+++ src/main/java/fr/miage/CamelRoute.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 public class CamelRoute extends RouteBuilder {
 
+    @SuppressWarnings("unchecked")
     public void configure() throws Exception {
 
         from("file:data/input?noop=true")
@@ -21,7 +22,7 @@ public class CamelRoute extends RouteBuilder {
 
             .marshal().json()
 
-            .to("file:data/output");
+        .to("file:data/output");
     }
 
     public static void main(String[] args) throws Exception {
